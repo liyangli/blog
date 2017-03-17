0e348629183884e89ab27c6a59983b732f5d4957
liyangli <ylisoftware@163.com>
2017-03-18 00:39:59 +0800

    设定对应socket是否有效

--- javascripts/compents/index.js
+++ javascripts/compents/index.js
@@ -5,10 +5,31 @@
  */
 "use strict";
 
-console.info("===============================");
+Vue.component('todo-item',{
+    props: ['names'],
+    template: '<ol><li v-for="name in names">{{name}}</li></ol>'
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello vue!'
+        message: 'hello vue!',
+        seen: true,
+        names: ['liyangli','lyl','liyl']
+    },
+    methods: {
+        changeMsg: function(){
+            sendMessage();
+        }
     }
 });
+
+//webSocket测试
+var ws = new WebSocket("ws://23.105.201.193:8181");
+ws.onopen = function(e){
+    console.log('Connection to server opened');
+};
+
+function sendMessage(){
+    ws.send("liyangli clicent send");
+}
